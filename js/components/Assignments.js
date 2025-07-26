9fde72bc9fed7123c2190c3eba8f039084b9818f
Anil Manandhar <mdhanil03@gmail.com>
2025-07-26 16:15:39 +0545

    handle form submission and parent child state communication step done

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,10 +1,13 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 export default {
-    components: { AssignmentList },
+    components: { AssignmentList,AssignmentCreate },
     template: ` <section class="space-y-6">
                     <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
-                    <assignment-list :assignments="filters.completed" title="In Progress"></assignment-list>
+                    <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+                    <assignment-create @add="add"></assignment-create>
                </section>
+              
                 `,
     data() {
         return {
@@ -12,7 +15,8 @@ export default {
                 { name: 'Finish Project', complete: false, id: 1 },
                 { name: 'Read Chapter four', complete: false, id: 2 },
                 { name: 'Turn in homework', complete: false, id: 3 },
-            ]
+            ],
+          
         };
     },
     computed: {
@@ -21,6 +25,16 @@ export default {
                 inProgress: this.assignments.filter(assignment => !assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete)
             };
+        }
+    },
+    methods : {
+        add(name){         
+            this.assignments.push({
+                name:name,
+                completed:false,
+                id:this.assignments.length + 1
+            });
+           
         }
     }
 }
